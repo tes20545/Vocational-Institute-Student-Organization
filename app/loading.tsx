763320d34c7c9683d0795465c0aa99d06fa0a01b
Suwijak Srikhao <tes.20545@gmail.com>
2023-10-15 02:58:32 +0700

    final install nextjs + tailwind

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+function LoadingSkeleton() {
+    return 'loading...';
+}
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <LoadingSkeleton />
+}
---
+++ components/SectionContainer.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+export default function SectionContainer({ children }: Props) {
+  return (
+    <section className="mx-auto max-w-full px-4 sm:px-6 xl:max-w-7xl xl:px-0">{children}</section>
+  )
+}
